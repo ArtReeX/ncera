36095993a0c98d3be01fe2e0eee9ea4e936a278e
Arthur Lazarenko <30784593+Arthur-Lazarenko@users.noreply.github.com>
2018-11-15 18:22:41 +0300

    Мелкие правки кода.

--- core/pattern.js
+++ core/pattern.js
@@ -5,30 +5,32 @@ const { cfgNetwork } = require('../config');
 module.exports.create = (models, config = cfgNetwork) => {
   try {
     // обработка каждой модели
-    return models.filter(model => model.stockCharts.length).map(({ currency, stockCharts }) => ({
-      currency,
-      pattern: stockCharts.map(({ exchange, columns }) => {
-        const pattern = [];
-        for (
-          let count = 0;
-          count + config.pattern.input + config.pattern.output < columns.length;
-          count += 1
-        ) {
-          pattern.push({
-            input: columns
-              .slice(count, count + config.pattern.input)
-              .map(column => utilities.objectToArray(column)),
-            output: columns
-              .slice(
-                count + config.pattern.input,
-                count + config.pattern.input + config.pattern.output,
-              )
-              .map(column => utilities.objectToArray(column)),
-          });
-        }
-        return { exchange, pattern };
-      }),
-    }));
+    return models
+      .filter(model => model.stockCharts.length)
+      .map(({ currency, stockCharts }) => ({
+        currency,
+        pattern: stockCharts.map(({ exchange, columns }) => {
+          const pattern = [];
+          for (
+            let count = 0;
+            count + config.pattern.input + config.pattern.output < columns.length;
+            count += 1
+          ) {
+            pattern.push({
+              input: columns
+                .slice(count, count + config.pattern.input)
+                .map(column => utilities.objectToArray(column)),
+              output: columns
+                .slice(
+                  count + config.pattern.input,
+                  count + config.pattern.input + config.pattern.output,
+                )
+                .map(column => utilities.objectToArray(column)),
+            });
+          }
+          return { exchange, pattern };
+        }),
+      }));
   } catch (error) {
     throw new Error(`Невозможно создать шаблоны из стандартизированных шаблонов. ${error.message}`);
   }
